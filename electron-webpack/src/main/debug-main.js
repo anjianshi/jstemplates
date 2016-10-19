987a4e7fe67fa6076a41b250a2c2957eefc87fde
anjianshi <anjianshi@gmail.com>
2016-10-19 15:35:18 +0800

    清理代码

--- electron-webpack/src/main/debug-main.js
+++ electron-webpack/src/main/debug-main.js
@@ -1,7 +1,7 @@
 import { BrowserWindow, ipcMain } from 'electron'
 
 // 提供方便的调试功能。需要在 renderer 中进行配合才能工作。
-export function enhanceDebug(win) {
+export function enhanceDebug() {
     const F12 = 123
     const F5 = 116
     ipcMain.on('keydown', (e, keyCode) => {
