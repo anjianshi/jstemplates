faaf46515655e79206c57fac912e66284b18dc43
anjianshi <anjianshi@gmail.com>
2016-10-19 22:05:43 +0800

    支持接受 options，并传递给 es2015 preset； 更新版本号

--- babel-presets/react/index.js
+++ babel-presets/react/index.js
@@ -1,11 +1,25 @@
-module.exports = {
-    presets: [
-        require('babel-preset-latest'),
-        require('babel-preset-react'),
-        require('babel-preset-stage-1'),
-    ],
-    plugins: [
-        require('babel-plugin-transform-decorators-legacy').default,
-        require('jsx-control-statements'),
-    ],
+// 参考： https://github.com/babel/babel/blob/master/packages/babel-preset-es2015/src/index.js
+
+function preset(context, opts) {
+    return {
+        presets: [
+            [require('babel-preset-latest'), { es2015: opts }],
+            require('babel-preset-react'),
+            require('babel-preset-stage-1')
+        ],
+        plugins: [
+            require('babel-plugin-transform-decorators-legacy').default,
+            require('jsx-control-statements')
+        ]
+    }
 }
+
+var oldConfig = preset()
+module.exports = oldConfig
+
+Object.defineProperty(oldConfig, "buildPreset", {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: preset
+})
