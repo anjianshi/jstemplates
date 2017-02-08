699567f98b09a5dbd73e474ed4ad4f9f2a257125
anjianshi <anjianshi@gmail.com>
2017-02-08 21:46:39 +0800

    eslint-config: babel 环境下开启 es6 env

--- eslint-configs/babel/index.js
+++ eslint-configs/babel/index.js
@@ -6,6 +6,10 @@ module.exports = {
 
     parser: 'babel-eslint',
 
+    env: {
+        es6: true               // 此设定是有必要的，不然 eslint 会认为 Set / Map 等对象是不存在 / 不允许使用的
+    },
+
     rules: {
         'strict': 0,            // babel 会自动给代码加上 use strict;
     }
