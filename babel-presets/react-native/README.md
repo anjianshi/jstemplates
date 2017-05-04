a3f952d4603ffe6ca99156165733396a50a6ef7c
anjianshi <anjianshi@gmail.com>
2017-05-04 13:15:58 +0800

    新增 react-native preset

---
+++ babel-presets/react-native/README.md
@@ -0,0 +1,1 @@
+ReactNative 的基本 babel 配置 + decorator + jsx-control-statements
---
+++ babel-presets/react-native/index.js
@@ -0,0 +1,11 @@
+// 参考： https://github.com/facebook/react-native/tree/master/babel-preset
+
+module.exports = {
+    presets: [
+        require('babel-preset-react-native')
+    ],
+    plugins: [
+        require('babel-plugin-transform-decorators-legacy').default,
+        require('hg-jsx-control-statements')
+    ]
+}
